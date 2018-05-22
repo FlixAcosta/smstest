f34086c592e35f6a838e8f43db6efe798432f0b2
FlixAcosta <stop.pfr@gmail.com>
2018-05-21 19:10:35 -0500

    primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  msj:any = {}
+  
+
+  constructor(public navCtrl: NavController,
+    private sms: SMS) {
 
   }
 
+  send(){
+   /* console.log(this.msj);
+    console.log(this.msj.txt);
+    this.sms.send(this.msj.text,this.msj.num); */
+
+
+    var options={
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+      android: {
+           intent: ''  // Opens Default sms app
+          //intent: '' // Sends sms without opening default sms app
+        }
+    }
+      this.sms.send('6181003821', 'Mensaje de prueba',options)
+        .then(()=>{
+          alert("success");
+        },()=>{
+          alert("failed");
+        });
+        this.sms.send('6182134740', 'Mensaje de prueba',options)
+        .then(()=>{
+          alert("success");
+        },()=>{
+          alert("failed");
+        });
+        this.sms.send('6188007250', 'Mensaje de prueba',options)
+        .then(()=>{
+          alert("success");
+        },()=>{
+          alert("failed");
+        });
+        this.sms.send('6181510261', 'Mensaje de prueba',options)
+        .then(()=>{
+          alert("success");
+        },()=>{
+          alert("failed");
+        });
 }
+
+
+
+  }
+
+
